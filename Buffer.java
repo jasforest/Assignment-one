ccc73e4d5d48f38eaac9e2ebf0b546f32e36cfc1
nightowl1718 <111955838+nightowl1718@users.noreply.github.com>
2023-02-22 12:57:09 -0800

    Create Buffer.java

---
+++ Buffer.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Buffer {
+	
+	public static List<String> innerList = new ArrayList<>();
+	
+	public static List<List<String>> outerList = new ArrayList<>();
+	
+	int count;
+	
+	 void bufferFile() throws FileNotFoundException {
+		
+		BufferedReader br = null;
+		
+		String line = "";
+		
+		try {
+			
+			br = new BufferedReader(new FileReader("tags.csv"));
+			
+			line = br.readLine();
+			
+			while((line=br.readLine()) != null) {  //read the file and add them into innerlist and outerlist. 
+				
+				innerList.add(line.split(",")[2]); //separated by comma. 
+				
+				outerList.add(innerList);
+				
+			}
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+	
+		}
+		
+   }
+}
